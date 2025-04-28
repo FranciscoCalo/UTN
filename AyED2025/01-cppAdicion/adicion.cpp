16f66bddda292a7534c2444c7b03936ab098d20d
FranciscoCaloStrata <francisco.calo@strata-analytics.us>
2025-04-28 18:52:14 -0300

    trabajo 01-adicion primer commit

---
+++ AyED2025/01-cppAdicion/adicion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    // Se declaran las variables a y b como enteros
+    int a, b;
+    
+    std::cout<< "Ingresar primer numero entero a sumar:";
+    std::cin>> a;
+    std::cout<< "Ingresar segundo numero entero a sumar:";
+    std::cin>> b;
+    std::cout<< "La suma de " << a << " + " << b << " es: " << a + b;
+    
+}
---
+++ AyED2025/01-cppAdicion/readme.md
@@ -0,0 +1,6 @@
+# Ejercicio 01-Adicion
+
+## Problema
+
+Obtener del usuario dos números y mostrarle la suma.
+
