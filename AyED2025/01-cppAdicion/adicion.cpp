b9e035f4a745cc25427dc91bfb4c20b6cc592caa
FranciscoCalo <fcalo@frba.utn.edu.ar>
2025-04-28 19:01:08 -0300

    agregados comentarios en adicion.cpp

--- AyED2025/01-cppAdicion/adicion.cpp
+++ AyED2025/01-cppAdicion/adicion.cpp
@@ -5,10 +5,13 @@ int main(){
     // Se declaran las variables a y b como enteros
     int a, b;
     
+    // Se solicita al usuario que ingrese dos números enteros y se almacenan en las variables a y b
     std::cout<< "Ingresar primer numero entero a sumar:";
     std::cin>> a;
     std::cout<< "Ingresar segundo numero entero a sumar:";
     std::cin>> b;
+
+    // Se imprime la suma de a y b en la consola
     std::cout<< "La suma de " << a << " + " << b << " es: " << a + b;
     
 }
